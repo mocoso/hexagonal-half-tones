7ebfa3cde1bb167fe4938a914c0d7ccce007252b
Joel Chippindale <joel@joelchippindale.com>
2020-01-02 12:24:43 +0000

    Get basic set up for webpack project

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require("path");
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(html)$/i,
+        loader: 'file-loader',
+        options: {
+          name: '[path][name].[ext]',
+        },
+      }
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js"
+  }
+};
+
